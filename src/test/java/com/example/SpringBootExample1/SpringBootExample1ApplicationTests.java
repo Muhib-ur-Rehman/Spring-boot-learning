011d8e1fc8bd58ab00051554c9368f1a36d29115
Muhib_ur_Rehman <mrehman@nisum.com>
2021-09-27 20:10:03 +0500

    secondCommit

--- src/test/java/com/example/SpringBootExample1/SpringBootExample1ApplicationTests.java
+++ src/test/java/com/example/SpringBootExample1/SpringBootExample1ApplicationTests.java
@@ -1,13 +1,5 @@
 package com.example.SpringBootExample1;
-import com.example.SpringBootExample1.service.StudntService;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@ContextConfiguration(classes = SpringBootExample1Application.class)
-@WebMvcTest(StudntService.class)
 class SpringBootExample1ApplicationTests {
 
 }
